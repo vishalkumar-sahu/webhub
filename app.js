2b7c22e0be79c25d629daeebc5242f6a8fb9579d
vishalkumar-sahu <vishal.sahu@thrilleasy.com>
2022-10-19 01:32:29 +0530

    removing port

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ if( process.env.NODE_ENV === "production" ){
     app.use(express.static("client/build"));
 }
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Connected on port : ${PORT}`);
 });
